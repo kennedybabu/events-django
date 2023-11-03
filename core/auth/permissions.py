511226c5989854db039ededc5a23668a6e95613f
kennedybabu <kennedynjogu8@gmail.com>
2023-11-03 07:40:07 +0300

    feature: delete a comment

--- core/auth/permissions.py
+++ core/auth/permissions.py
@@ -5,12 +5,22 @@ class UserPermission(BasePermission):
         if request.user.is_anonymous:
             return request.method in SAFE_METHODS
         
-        if view.basename in ['post']:
+        if view.basename in ['event']:
             return bool(request.user and request.user.is_authenticated)        
-        return False 
+        # return False 
     
+        if view.basename in ['event-comment']:
+            if request.method in ['DELETE']:
+                return bool(request.user.is_superuser or request.user in [obj.author, obj.event.author])
+            return bool(request.user and request.user.is_authenticated)
+        
+        if view.basename in ['user']:
+            if request.method in SAFE_METHODS:
+                return True 
+            return bool(request.user.id == obj.id)
+        
     def has_permission(self, request, view):
-        if view.basename in ["post"]:
+        if view.basename in ["event"]:
             if request.user.is_anonymous:
                 return request.method in SAFE_METHODS 
             
